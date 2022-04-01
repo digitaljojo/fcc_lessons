5aac22afcee0a3cd97b96b349f13c8c120dbf52b
Jonathan  Johnny <99233858+digitaljojo@users.noreply.github.com>
2022-03-31 23:51:20 -0700

    Update addTogether.js
    Figured out I needed to utilize currying to get the second parameter passed in. The other problem was utilizing another else-if statement in order to deal with multiple arguments passed in the first function.

--- addTogether.js
+++ addTogether.js
@@ -1,31 +1,47 @@
-function addTogether(...args) {
 let cl = (x) => console.log(x);
 
-for(let x in args){
-      if(typeof args[x] != 'number'){
-        cl("bad");
+function curry(sum){
+
+return function curried(a) {
+  //cl(arguments)
+  if(Number.isInteger(a) && arguments.length <2){ //Verifies if a is a single number
+    cl("So far so good: "+ a);
+    return function next(b){ 
+      cl("Maybe... "+ b);
+      if(Number.isInteger(b)){ //Verifies if b is a number
+        cl(a+b)
+        return sum(a,b);
+      }else{
+        cl("Soiled it "+ b);
         return undefined;
-      }
-    }
-  
-  if(arguments.length <2){
-    return addOn();
+          }
+    };
+  }else if(Number.isInteger(arguments[0]) && Number.isInteger(arguments[1])){
+    //If two arguments passed into a, verify both numbers. else undefined
+    cl("Here")
+    cl(arguments[0] + arguments[1]);
+    return arguments[0] + arguments[1];
   }else{
+    cl("Nope")
+    return undefined;
+  }
     
   }
- //cl(args)
-  cl("----");
-
-  function addOn(...args){
-    cl("okie");
-  }
-
-
-
 }
 
-addTogether(2,3);
+function sum(a, b) {
+  cl(a+b);
+  return a + b;
+}
+
+let addTogether = curry(sum);
+
+
+
+addTogether(2, 3); 
 addTogether(23, 30);
-addTogether(5)(7)
-//addTogether(2, "3");
-//addTogether(2)([3]);
+addTogether(5)(7);
+addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+addTogether(2, "3");
+addTogether(2)([3]);
+addTogether("2", 3);
